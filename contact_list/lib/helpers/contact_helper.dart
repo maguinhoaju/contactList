d71be7266e814fa710ab31dd6267feeea663427b
Maguinho <maguinho.aju@gmail.com>
2021-05-20 07:54:58 -0300

    DataBase Domain

---
+++ contact_list/lib/helpers/contact_helper.dart
@@ -0,0 +1,41 @@
+String idColumn = "idColumn";
+String nameColumn = "nameColumn";
+String emailColumn = "emailColumn";
+String phoneColumn = "phoneColumn";
+String imgColumn = "imgColumn";
+
+class ContactHelper {}
+
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String image;
+
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    image = map[imgColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: image
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, image: $image)";
+  }
+}
